a940179b1797ed0ac6cea09953071ba00bb6c5aa
00bs Station <return0000000@gmail.com>
2020-12-24 13:54:02 +0100

    Reconsider  removeStreamObject()

--- src/main/java/markettap/api_exchange/models/BaseModel.java
+++ src/main/java/markettap/api_exchange/models/BaseModel.java
@@ -27,5 +27,27 @@ public class BaseModel {
     public void addStreamObject(StreamObject str){
         coinList.add(str);
     }
+
+    /*
+        TODO: the removeStreamObject is not correct, need to use equals() instanceOf() methods
+        Example:
+        Probably use method chaing something like:
+
+            coinlist.remove(equals(StreamObject str){
+                ..code beneath here..
+            })
+
+            public boolean equals(Object obj) {
+                if (obj == null) {return false};
+                if (obj == this) {return true};
+                if (!(obj instanceof ArrayTest)) {return false};
+                ArrayTest o = (ArrayTest) obj;
+                return o.i == this.i;
+        }
+    */
+
+    public void removeStreamObject(StreamObject str){
+        coinList.remove(str);
+    }
     
 }
