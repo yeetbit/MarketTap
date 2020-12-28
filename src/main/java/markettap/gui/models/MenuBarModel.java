d263892963e51a91e85ea670c3dc738abb1dd0ea
Sebastian Oberdorfer <return0000000@gmail.com>
2020-12-28 21:33:57 +0100

    Needs more flexibility

--- src/main/java/markettap/gui/models/MenuBarModel.java
+++ src/main/java/markettap/gui/models/MenuBarModel.java
@@ -3,21 +3,36 @@ package markettap.gui.models;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import java.awt.Color;
+import java.util.ArrayList;
 
 public class MenuBarModel extends JMenuBar{
     
     private static final long serialVersionUID = -1965926954804999754L;
 
     private JMenu edit;
-    private JMenuItem taps;
-    private JMenuItem warnings;
-    private JMenuItem importExport;
+    private JMenu taps;
+    private JMenuItem openTap;
+    private JMenuItem changeTap;
+    private JMenuItem closeTap;
+    private JMenuItem createNewTap;
+    private JMenuItem modifyConfig;
+
+    private JMenu warnings;
+    private JMenuItem newWarning;
+    private JMenuItem changeWarning;
+    private JMenuItem removeWarning;
+    private JMenu importExport;
     private JMenuItem preferences;
     private JMenuItem exit;
     
     private JMenu view;
-    private JMenuItem theme;
-    private JMenuItem window;
+    private JMenu theme;
+    private JMenuItem a;
+    private JMenuItem b;
+    private JMenu window;
+    private JMenuItem larger;
+    private JMenuItem smaller;
 
     private JMenu help;
     private JMenuItem welcome;
@@ -26,34 +41,70 @@ public class MenuBarModel extends JMenuBar{
     private JMenuItem about;
 
 
+    private ArrayList<MenuModel> menus;
+    private ArrayList<MenuItemModel> items;
+
 
     public MenuBarModel(){
 
+        this.menus = new ArrayList<MenuModel>();
+        this.items = new ArrayList<MenuItemModel>();
+
+
         
         edit = new JMenu("edit");
-        taps = new JMenuItem("taps");
-        warnings = new JMenuItem("warnings");
-        importExport = new JMenuItem("import export");
-        preferences = new JMenuItem("preferences");
+        taps = new JMenu("taps");
+        openTap = new JMenuItem("open");
+        changeTap = new JMenuItem("modify");
+        closeTap = new JMenuItem("delete");
+        createNewTap = new JMenuItem("new entry");
+        modifyConfig = new JMenuItem("edit connections");
+
+        warnings = new JMenu("warnings");
+        newWarning = new JMenuItem("new");
+        changeWarning = new JMenuItem("modify");
+        removeWarning = new JMenuItem("delete");
+    
+        importExport = new JMenu("import export");
+        preferences = new JMenu("preferences");
         exit = new JMenuItem("exit");
 
         view = new JMenu("view");
-        theme = new JMenuItem("theme");
-        window = new JMenuItem("window");
-
+        theme = new JMenu("theme");
+        a = new JMenuItem("theme A");
+        b = new JMenuItem("theme B");
+        window = new JMenu("window");
+        larger = new JMenuItem("larger");
+        smaller = new JMenuItem("smaller");
+    
         help = new JMenu("help");
         welcome = new JMenuItem("welcome");
         debug = new JMenuItem("debug");
         chat = new JMenuItem("chat");
         about = new JMenuItem("about");
 
+        taps.add(openTap);
+        taps.add(changeTap);
+        taps.add(closeTap);
+        taps.add(createNewTap);
+        taps.add(modifyConfig);
         edit.add(taps);
+
+        warnings.add(newWarning);
+        warnings.add(changeWarning);
+        warnings.add(removeWarning);
         edit.add(warnings);
+
         edit.add(importExport);
         edit.add(preferences);
         edit.add(exit);
-
+        
+        theme.add(a);
+        theme.add(b);
         view.add(theme);
+
+        window.add(larger);
+        window.add(smaller);
         view.add(window);
 
         help.add(welcome);
@@ -65,7 +116,74 @@ public class MenuBarModel extends JMenuBar{
         this.add(help);
 
     }
+    public void setName(String name){
+        this.setName(name);
+    }
+
+    public String getName(){
+        return this.getName();
+    }
+
+    public void setBackground(String bgHex){
+        this.setBackground(Color.decode(bgHex));
+    }
+
+    public void setMenus(MenuModel m){
+        this.menus.add(m);
+    }
+
+    public class MenuModel extends JMenu{
+        
+        private ArrayList<MenuModel> menus;
+        private ArrayList<MenuItemModel> items;
+
+        public MenuModel(){
+            this.menus = new ArrayList<MenuModel>();
+            this.items = new ArrayList<MenuItemModel>();
+        }
+
+        private static final long serialVersionUID = 1L;
+
+        public void setName(String name){
+            this.setName(name);
+        }
+
+        public String getName(){
+            return this.getName();
+        }
+
+        public void setBackground(String bgHex){
+            this.setBackground(Color.decode(bgHex));
+        }
+
+        public void setMenus(MenuModel m){
+            this.add(m);
+        }
+
+        public void setItems(String item){
+            
+            this.add(item);
+            
+        }
+    }
+
+    public class MenuItemModel extends JMenuItem{
+
+        private static final long serialVersionUID = 1L;
+
+        public void setName(String name){
+            this.setName(name);
+        }
+
+        public String getName(){
+            return this.getName();
+        }
+
+        public void setBackground(String bgHex){
+            this.setBackground(Color.decode(bgHex));
+        }
 
 
+    }
     
 }
