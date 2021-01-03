9e9fa9f07982af77c03f479f8b5f4b4cb8dbcfaf
Sebastian Oberdorfer <return0000000@gmail.com>
2021-01-03 19:51:04 +0100

    Added Enum constants

---
+++ src/main/java/markettap/gui/constants/MenuConst.java
@@ -0,0 +1,44 @@
+package markettap.gui.constants;
+
+public enum MenuConst {
+
+    edit{ "1",  }
+
+
+
+
+    // edit = new MenuModel("edit");
+    // taps = new MenuModel("taps");
+    // openTap = new MenuModel("open");
+    // changeTap = new MenuItemModel("modify");
+    // closeTap = new MenuItemModel("delete");
+    // createNewTap = new MenuItemModel("new entry");
+    // modifyConfig = new MenuItemModel("edit connections");
+
+    // warnings = new MenuModel("warnings");
+    // newWarning = new MenuItemModel("new");
+    // changeWarning = new MenuItemModel("modify");
+    // removeWarning = new MenuItemModel("delete");
+
+    // importExport = new MenuModel("import export");
+    // preferences = new MenuItemModel("preferences");
+    // exit = new MenuItemModel("exit");
+
+    // view = new MenuModel("view");
+    // theme = new MenuModel("theme");
+    // a = new MenuItemModel("theme A");
+    // b = new MenuItemModel("theme B");
+    // window = new MenuModel("window");
+    // larger = new MenuItemModel("larger");
+    // smaller = new MenuItemModel("smaller");
+
+    // help = new MenuModel("help");
+    // welcome = new MenuItemModel("welcome");
+    // debug = new MenuItemModel("debug");
+    // chat = new MenuItemModel("chat");
+    // about = new MenuItemModel("about");
+
+
+    
+    
+}
