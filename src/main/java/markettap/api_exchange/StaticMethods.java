10948b69b9e946ed1d2068934f3c25377c35e331
00bs Station <return0000000@gmail.com>
2021-02-16 17:08:17 +0100

    comment

--- src/main/java/markettap/api_exchange/StaticMethods.java
+++ src/main/java/markettap/api_exchange/StaticMethods.java
@@ -2,7 +2,13 @@ package markettap.api_exchange;
 
 public class StaticMethods {
 
-    
+    /* 
+        change method accepts 2 double variables,
+        and returns an array with differenc between old and new price
+        change[0] == factor
+        change[1] == percentage
+
+    */
     public static Double[] change(Double oldPrice, Double newPrice) {
         Double change[]= {-1.0,-1.0};
         change[0] = (newPrice-oldPrice)/oldPrice;
