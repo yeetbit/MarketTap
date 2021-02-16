b34976c89c7d86728d69fad3e7c6777b9afd3c2e
00bs Station <return0000000@gmail.com>
2021-02-16 17:04:45 +0100

    math-method change calculator

--- src/main/java/markettap/api_exchange/StaticMethods.java
+++ src/main/java/markettap/api_exchange/StaticMethods.java
@@ -2,9 +2,14 @@ package markettap.api_exchange;
 
 public class StaticMethods {
 
+    
+    public static Double[] change(Double oldPrice, Double newPrice) {
+        Double change[]= {-1.0,-1.0};
+        change[0] = (newPrice-oldPrice)/oldPrice;
+        change[1] = change[0]*100;
 
-    public static void percent(Double oldPrice, Double lastPrice) {
-        
+        return change;
+
     }
 
 
